27beef9845d0fb971f5fc978bf443f0fd1787abb
Nikhil Narayanan <nikhilnarayanan2007@gmail.com>
2020-04-11 12:25:13 +0530

    initialising a dictionary

---
+++ python/programming:basics/dictionary.py
@@ -0,0 +1,9 @@
+# creating an empty dictionary
+fruits_dict = {}
+
+# creating an dictionary with values in it
+fruits_dict = { 'apple': 2, 'orange': 4 }
+
+# list and other dictionaries
+fruits_dict = { 'apple': 2, 'orange': 4, 'dry_fruits': ['walnut','apricot','blueburry'], 'grape': {'dry': 10, 'normal': 20} }
+
